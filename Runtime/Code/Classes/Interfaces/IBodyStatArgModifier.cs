7d424ddb5ca7a73bc458b0a586ee6e65822b580c
Nebby <nebby131999@gmail.com>
2022-07-04 18:08:44 -0400

    Documentation - 3
    Interfaces

--- Runtime/Code/Classes/Interfaces/IBodyStatArgModifier.cs
+++ Runtime/Code/Classes/Interfaces/IBodyStatArgModifier.cs
@@ -2,8 +2,16 @@
 
 namespace Moonstorm
 {
+    /// <summary>
+    /// An IBodyStatArgModifier can be used to modify the Stats of a characterbody using <see cref="RecalculateStatsAPI"/>
+    /// </summary>
     public interface IBodyStatArgModifier
     {
+        /// <summary>
+        /// Modify the stat arguments for this body.
+        /// NEVER override, only add, substract, divide or multiply.
+        /// </summary>
+        /// <param name="args">The arguments to modify</param>
         void ModifyStatArguments(RecalculateStatsAPI.StatHookEventArgs args);
     }
 }
