479451e137866c0bbfd23526fc2d4c7aa5ee191f
Nebby <nebby131999@gmail.com>
2022-07-05 22:48:43 -0400

    copy over xml doc

---
+++ Editor/Code/CopyXMLDoc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+using RoR2EditorKit.Common;
+
+namespace Moonstorm.EditorUtils
+{
+    [InitializeOnLoad]
+    internal static class CopyXMLDoc
+    {
+        static CopyXMLDoc()
+        {
+            if(ShouldCopy())
+            {
+                Debug.Log($"Copying over the MSU XML Doc");
+                DoCopy();
+            }
+        }
+
+        private static bool ShouldCopy()
+        {
+            var relativePath = AssetDatabase.GUIDToAssetPath("ded440f4e5e23cd4a8bbfb38e5f13ebf");
+            var fullPath = Path.GetFullPath(relativePath);
+            var fileName = Path.GetFileName(fullPath);
+            var pathToCheck = Path.Combine(Constants.FolderPaths.ScriptAssembliesFolder, fileName);
+            return !File.Exists(pathToCheck);
+        }
+
+        private static void DoCopy()
+        {
+            var relativePath = AssetDatabase.GUIDToAssetPath("ded440f4e5e23cd4a8bbfb38e5f13ebf");
+            var sourcePath = Path.GetFullPath(relativePath);
+            var fileName = Path.GetFileName(sourcePath);
+            var destPath = Path.Combine(Constants.FolderPaths.ScriptAssembliesFolder, fileName);
+            File.Copy(sourcePath, destPath, true);
+        }
+    }
+}
