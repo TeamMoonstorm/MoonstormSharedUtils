574bc636133f89d8697765e09a60b075d2c655b2
Nebby <nebby131999@gmail.com>
2024-03-04 20:43:08 -0300

    bweh pt2

--- Runtime/Code/ParallelCoroutineHelper.cs
+++ Runtime/Code/ParallelCoroutineHelper.cs
@@ -1,9 +1,10 @@
+using R2API.Utils;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
 namespace MSU
-{
+{   
     public class ParallelCoroutineHelper
     {
         private List<Wrapper> _wrappers = new List<Wrapper>();
@@ -12,8 +13,49 @@ namespace MSU
         {
             _wrappers.Add(new Wrapper
             {
-                coroutineMethod = func
+                @delegate = func
             });
+        }
+
+        public void Add(Delegate @delegate, object[] args)
+        {
+            if (!IsSafe(@delegate, args))
+                return;
+
+            _wrappers.Add(new Wrapper
+            {
+                @delegate = @delegate,
+                args = args,
+            });
+        }
+
+        private bool IsSafe(Delegate @delegate, object[] args)
+        {
+            var methodInfo = @delegate.Method;
+
+            var returnType = methodInfo.ReturnType;
+            if (returnType == null || returnType == typeof(void))
+                return false;
+
+            if (!methodInfo.ReturnType.IsSameOrSubclassOf<IEnumerator>())
+            {
+                return false;
+            }
+
+            var parameters = methodInfo.GetParameters();
+            if (parameters.Length != args.Length)
+                return false;
+
+            for(int i = 0; i < parameters.Length; i++)
+            {
+                var argType = args[i].GetType();
+                if (!(parameters[i].ParameterType == argType || parameters[i].ParameterType.IsSubclassOf(argType)))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public void Start()
@@ -36,12 +78,13 @@ namespace MSU
 
         private class Wrapper
         {
-            public Func<IEnumerator> coroutineMethod;
+            public Delegate @delegate;
+            public object[] args;
             public IEnumerator coroutine;
 
             public void Start()
             {
-                coroutine = coroutineMethod.Invoke();
+                coroutine = (IEnumerator)@delegate.DynamicInvoke(args);
             }
 
             public bool IsDone
