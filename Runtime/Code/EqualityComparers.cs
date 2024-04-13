95791ed92cc7613ea411a811e4b1cd2a4e0fe57e
Nebby <nebby131999@gmail.com>
2024-04-13 17:43:41 -0400

    Foirgot this

---
+++ Runtime/Code/EqualityComparers.cs
@@ -0,0 +1,28 @@
+﻿using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSU
+{
+    public struct CharacterBodyIndexEqualityComparer : IEqualityComparer<CharacterBody>
+    {
+        public bool Equals(CharacterBody x, CharacterBody y)
+        {
+            if (!x || !y)
+                return false;
+
+            if (x.bodyIndex == BodyIndex.None || y.bodyIndex == BodyIndex.None)
+                return false;
+
+            return x.bodyIndex == y.bodyIndex;
+        }
+
+        public int GetHashCode(CharacterBody obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
