a93dc7a75a957c0045426117f2be3eac8108f8cb
Nebby <nebby131999@gmail.com>
2024-09-16 18:46:58 -0300

    :3splosion:

--- Runtime/Code/ContentInterfaces/DefaultInterfaces/IVanillaSurvivorContentPiece.cs
+++ Runtime/Code/ContentInterfaces/DefaultInterfaces/IVanillaSurvivorContentPiece.cs
@@ -5,8 +5,7 @@ namespace MSU
 {
     public interface IVanillaSurvivorContentPiece : IContentPiece
     {
-        SurvivorDef survivorDef { get; set; }
-        string survivorDefAddress { get; }
+        SurvivorDef survivorDef { get; }
 
         IEnumerator InitializeAsync();
     }
